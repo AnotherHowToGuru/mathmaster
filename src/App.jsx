654b8b9c2f6c8d8366b6e7abfacb6f9e63d3933a
AnotherHowToGuru <anotherhowtoguru@gmail.com>
2025-06-19 23:42:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ChildDashboard from './pages/ChildDashboard';
 import ParentDashboard from './pages/ParentDashboard';
 import Login from './pages/Login';
@@ -12,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/child" element={<Navigate to="/child/dashboard" replace />} /> {/* Redirect /child to /child/dashboard */}
         <Route path="/child/dashboard" element={<ChildDashboard />} />
         <Route path="/parent/dashboard" element={<ParentDashboard />} />
         <Route path="/child/topic/:topicId" element={<TopicLessons />} />
